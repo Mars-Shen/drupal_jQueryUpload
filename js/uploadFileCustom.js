9d538e3566b9e2f4bd6e13f4857643001c46f909
Mars Shen <mars.shen@perficient.com>
2015-05-04 02:39:21 -0400

    fix some issue and do some improvement

--- js/uploadFileCustom.js
+++ js/uploadFileCustom.js
@@ -1,4 +1,4 @@
-var callbackID = 0;
+var callbackID = null;
 
 (function ($) {
     Drupal.behaviors.jQueryUpload = {
@@ -12,21 +12,30 @@ var callbackID = 0;
                 },
                 start:function (e,data) {
                     $('#process_bar').css('width','0%');
+                    $('#process_bar_text').html('0%');
                     $('#process_bar_tip').html('uploading...');
                     $('#progress').show();
                     console.log('Uploads started');
                 },
                 done: function (e, data) {
                     clearInterval(callbackID);
-                    $('#process_bar').css('width','100%');
-                    $('#progress').hide();
+                    callbackID = null;
+                    if(data.result.status=='success'){
+                        $('#process_bar').css('width','100%');
+                        $('#progress').hide();
+                    }else{
+                        $('#process_bar_tip').html(data.result.msg);
+                    }
+
                     console.log('Uploads done');
                 },
                 progress:function (e, data) {
                     console.log(parseInt(data.loaded / data.total * 100, 10));
                         var process_number = parseInt(data.loaded / data.total * 50, 10);
                         if(process_number>=50){
-                            callBackToGetBackgroundProcessNumber();
+                            if(typeof callbackID === 'undefined' || callbackID === null ){
+                                callBackToGetBackgroundProcessNumber();
+                            }
                         }else{
                             $('#process_bar').css('width',process_number + '%');
                             $('#process_bar_text').html(process_number + '%');
@@ -44,11 +53,13 @@ var callbackID = 0;
         $.ajax({ 
             type:"POST", 
             url:"getProcessNumber", 
-            success:function(data){ 
+            success:function(data){
+                if(typeof callbackID !== 'undefined' && callbackID !== null ){
                 var process_number = 50 + parseInt(data.num) 
                 $('#process_bar').css('width',process_number+ '%');
                 $('#process_bar_text').html(process_number + '%');
                 $('#process_bar_tip').html(data.msg);
+                }
             }
 
         });
