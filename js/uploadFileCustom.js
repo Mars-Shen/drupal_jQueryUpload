cbe67a019ea5381f2cb98eb0550b10ba899cfb60
Mars Shen <mars.shen@perficient.com>
2015-05-04 03:32:29 -0400

    fix cannot find function issue

--- js/uploadFileCustom.js
+++ js/uploadFileCustom.js
@@ -1,4 +1,4 @@
-var callbackID = null;
+var callbackFlag = false;
 
 (function ($) {
     Drupal.behaviors.jQueryUpload = {
@@ -18,8 +18,7 @@ var callbackID = null;
                     console.log('Uploads started');
                 },
                 done: function (e, data) {
-                    clearInterval(callbackID);
-                    callbackID = null;
+                    callbackFlag = false;
                     if(data.result.status=='success'){
                         $('#process_bar').css('width','100%');
                         $('#progress').hide();
@@ -33,7 +32,8 @@ var callbackID = null;
                     console.log(parseInt(data.loaded / data.total * 100, 10));
                         var process_number = parseInt(data.loaded / data.total * 50, 10);
                         if(process_number>=50){
-                            if(typeof callbackID === 'undefined' || callbackID === null ){
+                            if(!callbackFlag){
+                                callbackFlag = true;
                                 callBackToGetBackgroundProcessNumber();
                             }
                         }else{
@@ -46,7 +46,7 @@ var callbackID = null;
     };
 
     function callBackToGetBackgroundProcessNumber(){
-        callbackID = setInterval(getNumber,500);
+        getNumber();
     }
 
     function getNumber(){
@@ -54,11 +54,16 @@ var callbackID = null;
             type:"POST", 
             url:"getProcessNumber", 
             success:function(data){
-                if(typeof callbackID !== 'undefined' && callbackID !== null ){
-                var process_number = 50 + parseInt(data.num) 
-                $('#process_bar').css('width',process_number+ '%');
-                $('#process_bar_text').html(process_number + '%');
-                $('#process_bar_tip').html(data.msg);
+                if(callbackFlag){
+                    var process_number = 50 + parseInt(data.num)
+                    $('#process_bar').css('width',process_number+ '%');
+                    $('#process_bar_text').html(process_number + '%');
+                    $('#process_bar_tip').html(data.msg);
+                }
+            },
+            complete: function(){
+                if(callbackFlag){
+                    setTimeout(getNumber,500)
                 }
             }
 
